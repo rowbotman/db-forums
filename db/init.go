5a418d25da6ba86b3db43e561bf833a94ced3452
Andrey Prokopenko <prokopenko.rower@mail.ru>
2020-06-13 12:25:50 +0300

    removed deprecated

--- db/init.go
+++ db/init.go
@@ -9,7 +9,6 @@ import (
 var db *pgxpool.Pool
 
 func init() {
-	//pgxConfig, _ := pgx.ParseURI("postgres://role1:12345@localhost:5432/docker")
 	config, err := pgxpool.ParseConfig("postgres://park:admin@localhost:5432/park_forum")
 	if err != nil {
 		log.Fatal(err.Error())
@@ -18,24 +17,6 @@ func init() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	//db, err = pgx.NewConnPool(
-	//	pgx.ConnPoolConfig{
-	//		ConnConfig: pgxConfig,
-	//	})
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	//initSql, err := ioutil.ReadFile("init.sql")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//initString := string(initSql)
-	//
-	//_, err = db.Exec(initString)
-	//if err != nil {
-	//	panic(err)
-	//}
 }
 
 func GetDB() *pgxpool.Pool {
